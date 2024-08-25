a621ba3ebe4d7cc1dab708c98867e3f14f6be88b
13rianSVargasC <brians.vargasc@konradlorenz.edu.co>
2024-08-24 20:44:19 -0500

    CodeForces to GitHub

---
+++ CodeForces/A - Team.py
@@ -0,0 +1,11 @@
+#A - Team
+n = int(input())
+int (sumt = 0)
+for i in range(n):
+    pa = int(input())
+    sa = int(input())
+    ta = int(input())
+    sum = pa+sa+ta
+    if sum >= 2:
+        sumt= sumt + 1
+print(sumt)
