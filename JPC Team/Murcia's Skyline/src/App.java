b77d15515172c88da5550e4b7ed2dda4cc9bf7b7
13rianVargas <bsvargasc13@gmail.com>
2024-08-27 03:12:10 -0500

    Almost There
    Falta encontrar el algoritmo correcto.

--- JPC Team/Murcia's Skyline/src/App.java
+++ JPC Team/Murcia's Skyline/src/App.java
@@ -4,57 +4,109 @@ public class App {
 
     static Scanner sc = new Scanner(System.in);
     static int edificios;
-    static int h[] = new int[edificios];
-    static int w[] = new int[edificios];
-    static int sumh = 0;
-    static int sumw = 0;
-    static int sumhMax = 0;
-    static int sumwMax = 0;
-    
+    static int sumaParcialUP;
+    static int sumaMaximaUP;
+    static int sumaParcialDOWN;
+    static int sumaMaximaDOWN;
+    static int cont = 1;
+
     public static void main(String[] args) {
 
         int casos = sc.nextInt();
-        testCases(casos);
-
-        String incre = "";
-        String decre = "";
-
-        for (int i = 0; i < edificios; i++) {
-            
-        }
-
         
+        testCases(casos);
 
         sc.close();
     }
 
     public static void testCases (int casos) {
-        if (casos  != 0) {
-            edificios =  sc.nextInt();
-            for (int i = 0; i < edificios; i++) {
-                h[i] = sc.nextInt();
-            }
-            for (int i = 0; i < edificios; i++) {
-                w[i] = sc.nextInt();
-            }
-        }else if (casos == 0) {
 
+        if (casos != 0) {
+
+            //variaables
+            int edificiosCreciendo = 1;
+            int edificiosDecreciendo = 1;
+            String skyline;
+            
+            //edificios, altura y anchura
+            edificios = sc.nextInt();
+            int high[] = new int[edificios];
+            int width[] = new int[edificios];
+            
+            //escribe array altura
             for (int i = 0; i < edificios; i++) {
-                sumh += h[i];
-                if (sumhMax  < sumh) {
-                    sumhMax = sumh;
-                }
+                high[i] = sc.nextInt();
             }
+            
+            //escribe array anchura
             for (int i = 0; i < edificios; i++) {
-                sumw += w[i];
-                if (sumwMax  < sumw) {
-                    sumwMax = sumw;
-                }
+                width[i] = sc.nextInt();  
             }
-        }
+
+
+            //doble for para recorrer matriz
+            for (int i = 0; i < edificios; i++) {
+
+                sumaParcialUP = width[i];
+                sumaParcialDOWN =  width[i];
+
+
+                for (int j = 1; j < edificios; j++) {
+
+                    
+                    if (high[i] < high[j]) {//increasing
+                        if (high[j] >  high[j--]) {
+                            sumaParcialUP += width[j--];
+                            if (high[j] <  high[j++]) {
+                                sumaParcialUP += width[j++];
+                            }
+                        }
+                        
+                        if (sumaMaximaUP < sumaParcialUP) {
+                            sumaMaximaUP = sumaParcialUP;
+                        }
+
+
+                    }
+                    // else if (high[i] > high[j]) {//decreasing
+
+
+                        
+
+                        
+                    // }//close ifs increasing & decreasing
+
+
+                    
+
+                    // if (sumaMaximaDOWN < sumaParcialDOWN) {
+                    //     sumaMaximaDOWN = sumaParcialDOWN;
+                    // }
+
+                }//close for2
+
+                // edificiosCreciendo = 1;
+                // edificiosDecreciendo = 1;
+
+            }//close for1
+
+            if (edificiosCreciendo>=edificiosDecreciendo){
+                skyline = "Increasing (" + sumaMaximaUP + "). Decreasing (" + sumaMaximaDOWN +").";
+            }else{
+                skyline = "Decreasing (" + sumaMaximaDOWN + "). Increasing (" + sumaMaximaUP +").";
+            }
+
+            System.out.println("Case " + cont + ". " + skyline);
+
+        }//close if cases
+
+        //variables de reset
+        sumaParcialUP = 0;
+        sumaParcialDOWN = 0;
         casos -= 1;
-        testCases(casos);
-    }
+        cont++;
 
+        testCases(casos);//recursividad
 
-}
+    }//close testCases
+}//close class
