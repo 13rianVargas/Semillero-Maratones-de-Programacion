5b7cb4ca1ca5091ef6004d21ce22d3bc2172d2d0
13rianVargas <bsvargasc13@gmail.com>
2024-08-26 21:01:10 -0500

    Update Recursividad
    Creo recursividad para evitar el for anidado

--- JPC Team/Murcia's Skyline/src/App.java
+++ JPC Team/Murcia's Skyline/src/App.java
@@ -2,25 +2,59 @@ import java.util.*;
 
 public class App {
 
+    static Scanner sc = new Scanner(System.in);
+    static int edificios;
+    static int h[] = new int[edificios];
+    static int w[] = new int[edificios];
+    static int sumh = 0;
+    static int sumw = 0;
+    static int sumhMax = 0;
+    static int sumwMax = 0;
+    
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int test = sc.nextInt();
-        int edif = sc.nextInt();
+        int casos = sc.nextInt();
+        testCases(casos);
 
-        int[] Largo = null;
+        String incre = "";
+        String decre = "";
 
-        for (int i = 0; i < edif; i++) {
-            Largo[i] = sc.nextInt();
+        for (int i = 0; i < edificios; i++) {
+            
         }
 
-        int[] Ancho = null;
-
-        for (int i = 0; i < edif; i++) {
-            Ancho[i] = sc.nextInt();
-        }
+        
 
         sc.close();
     }
+
+    public static void testCases (int casos) {
+        if (casos  != 0) {
+            edificios =  sc.nextInt();
+            for (int i = 0; i < edificios; i++) {
+                h[i] = sc.nextInt();
+            }
+            for (int i = 0; i < edificios; i++) {
+                w[i] = sc.nextInt();
+            }
+        }else if (casos == 0) {
+
+            for (int i = 0; i < edificios; i++) {
+                sumh += h[i];
+                if (sumhMax  < sumh) {
+                    sumhMax = sumh;
+                }
+            }
+            for (int i = 0; i < edificios; i++) {
+                sumw += w[i];
+                if (sumwMax  < sumw) {
+                    sumwMax = sumw;
+                }
+            }
+        }
+        casos -= 1;
+        testCases(casos);
+    }
+
 
 }
