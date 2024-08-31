94a8b8f77ac55dfcc80eef9988b864fd16aa6db5
13rian Vargas <bsvargasc13@gmail.com>
2024-08-31 13:06:44 -0500

    Solved #include <bits/stdc++.h>

--- VJudge/MaxSubarraySum.cpp
+++ VJudge/MaxSubarraySum.cpp
@@ -1,18 +1,24 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
+#define ll long long
+
 int main(){
-    long long n;
-    cin>>n;
-    long long x[n];
-    for (int i = 0; i < n; i++){
+    
+    ll num;
+    cin >> num;
+    
+    vector <ll> x(num);
+
+    for (int i = 0; i < num; i++){
         cin>>x[i];
     }
     
-    long long sum = x[0];
-    long long max = x[0];
+    ll sum = x[0];
+    ll max = x[0];
     
-    for (int i = 1; i < n; i++){
+    for (int i = 1; i < num; i++){
         sum += x[i];
         if (x[i] > sum){
             sum = x[i];
