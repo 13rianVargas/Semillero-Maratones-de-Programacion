37b72f8e33b559c708b93d46d38cb5c66c188aba
13rian Vargas <bsvargasc13@gmail.com>
2024-09-06 22:58:17 -0500

    A-Team Solution

--- CodeForces/A - Team.py
+++ CodeForces/A - Team.py
@@ -1,11 +1,9 @@
 #A - Team
 n = int(input())
-int (sumt = 0)
+sumt = 0
 for i in range(n):
-    pa = int(input())
-    sa = int(input())
-    ta = int(input())
-    sum = pa+sa+ta
+    a1, a2, a3 = map(int, input().split())
+    sum = a1+a2+a3
     if sum >= 2:
         sumt= sumt + 1
 print(sumt)
